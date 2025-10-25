165d7f5f48e468823439a5690e37e468a2dd58b9
Piyush1532 <piyushadav503@gmail.com>
2025-10-25 16:21:24 +0530

    mounted components

---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Header from '../Components/Header'
+import BlogList from '../Components/BlogList'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+        <BlogList/>
+    </div>
+  )
+}
+
+export default Home
