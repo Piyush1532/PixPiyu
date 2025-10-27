0181dfbee4d810c8eca608ab86b63889ed8944e2
Piyush1532 <piyushadav503@gmail.com>
2025-10-27 15:12:04 +0530

    mounted component

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
 import BlogList from '../Components/BlogList'
+import NewsLetter from '../Components/NewsLetter'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
         <Navbar/>
         <Header/>
         <BlogList/>
+        <NewsLetter/>
+        <Footer/>
     </div>
   )
 }
