cd4d765efa1b36c782fa7a350eccffa3817db4cd
Piyush1532 <piyushadav503@gmail.com>
2025-10-28 14:35:09 +0530

    created new routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Blog from './Pages/Blog'
+import Layout from './Pages/Admin/Layout'
+import Dashboard from './Pages/Admin/Dashboard'
+import AddBlog from './Pages/Admin/AddBlog'
+import ListBlog from './Pages/Admin/ListBlog'
+import Comments from './Pages/Admin/Comments'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/blog/:id" element={<Blog/>}/>
+        <Route path="/blog/:blogId" element={<Blog/>}/>
+
+        <Route path='/admin'element={<Layout/>}>
+<Route index element={<Dashboard/>}/>
+<Route path="addblog" element={<AddBlog/>}/>
+<Route path="listblog" element={<ListBlog/>}/>
+<Route path="comments" element={<Comments/>}/>
+        </Route>
       </Routes>
     </div>
   )
