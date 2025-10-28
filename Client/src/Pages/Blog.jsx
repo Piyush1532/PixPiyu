ca6ccfe61ae0bdfa9d5ff455e600303f7c269a06
Piyush1532 <piyushadav503@gmail.com>
2025-10-28 14:36:34 +0530

    created design and functionality

--- Client/src/Pages/Blog.jsx
+++ Client/src/Pages/Blog.jsx
@@ -1,30 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, blog_data } from '../assets/assets'
+import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../Components/Navbar'
 import Moment from "moment"
+import Footer from '../Components/Footer'
+import Loader from '../Components/Loader'
+
 const Blog = () => {
 
- const {id}=useParams()
+ const {blogId}=useParams()
 
 const [data,SetData]=useState(null)
+const [comments,SetComments]=useState([])
+
+const [name,SetName]=useState("")
+const [content,SetContent]=useState("")
+
 
 const fetchBlogData=async () => {
-  const data=blog_data.find(item=>item._id===id)
+  const data=blog_data.find(item=>item._id===blogId)
   SetData(data)
 }
 
+const fetchComments=async () => {
+  SetComments(comments_data)
+}
+
+const addComment=async (evt) => {
+  evt.preventDefault
+}
+
+
+
 useEffect(()=>{
 fetchBlogData()
+fetchComments()
 },[])
+
+
   return data ? (
     <div className='relative'>
       <img src={assets.gradientBackground} alt=""  className='absolute -top-30 -z-1 opacity-50'/>
       <Navbar/>
-<div className='text-center mt-20 text-gray-600'>
+<div className='text-center mt-10 text-gray-600'>
   <p className='text-[#3EA6A9] py-4 font-medium'>Published on {Moment(data.createdAt).format("do MMMM YYYY")}</p>
   <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
-  <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+  <h2 className='my-5 max-w-lg truncate mx-auto' dangerouslySetInnerHTML={{__html:data.subTitle}}></h2>
   <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-[#3EA6A9]/35 bg-[#3EA6A9]/20 font-medium text-black/60'>Piyush A</p>
 </div>
 
@@ -33,9 +54,50 @@ fetchBlogData()
 
 <div dangerouslySetInnerHTML={{__html:data.description}} className='rich-text max-w-3cl mx-auto '></div>
 
+{/* Comment section */}
+<div className='mt-14 mb-10 max-w-3xl mx-auto'>
+<p className='font-semibold mb-4'>Comments {comments.length}</p>
+<div className='flex flex-col gap-4'>
+{comments.map((item,index)=>(
+  <div key={index} className='relative bg-[#3ea6a9]/2 border border-[#3ea6a9]/5  max-w-xl p-4 rounded text-gray-600'>
+<div className='flex items-center gap-2 mb-2'>
+  <img src={assets.user_icon} alt=""  className='w-6'/>
+  <p className='font-medium'>{item.name}</p>
 </div>
+<p className='text-sm max-w-md ml-8'>{item.content}</p>
+<div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+  </div>
+))}
+</div>
+</div>
+
+
+{/* Add comments section  */}
+<div className='max-w-3xl mx-auto'>
+<p className='font-semibold mb-4'>Add Your Comment</p>
+<form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+
+<input type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none' onChange={(e)=>SetName(e.target.value)} value={name}/>
+<textarea placeholder='Comment' required className='w-full p-2 rounded outline-none h-48  border border-gray-300 ' onChange={(e)=>SetContent(e.target.value)} value={content} ></textarea>
+<button type='submit' className='bg-[#3EA6A9] text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+</form>
+</div>
+
+{/* share btns */}
+<div className='my-24 max-w-3xl mx-auto'>
+<p className='font-semibold my-4 '>Share this articles on social media </p>
+<div className="flex">
+  <img src={assets.facebook_icon} alt="" width={50}/>
+  <img src={assets.twitter_icon} alt="" width={50}/>
+  <img src={assets.googleplus_icon} alt="" width={50}/>
+</div>
+</div>
+
+
+</div>
+<Footer/>
     </div>
-  ):<div>Loading...</div>
+  ):<Loader/>
 }
 
 export default Blog
