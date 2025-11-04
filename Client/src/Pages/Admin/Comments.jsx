74e96c927e0f0bdbd424812187d5faa3cb34967e
Piyush1532 <piyushadav503@gmail.com>
2025-11-04 14:56:58 +0530

    comment page design

---
+++ Client/src/Pages/Admin/Comments.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { comments_data } from '../../assets/assets'
+import CommentTable from '../../Components/Admin/CommentTable'
+
+const Comments = () => {
+const [comments,SetComments]=useState([])
+const [filter,SetFilter]=useState("Not Approved")
+
+const fetchComments=async () => {
+  SetComments(comments_data)
+}
+
+useEffect(()=>{
+fetchComments()
+},[])
+
+  return (
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/20'>
+      <div className='flex justify-between items-center max-w-3xl'>
+<h1>Comments</h1>
+<div className='flex gap-4'>
+<button onClick={()=>SetFilter("Approved")} className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter ==='Approved'? "text-[#3EA6A9]": "text-gray-700"}`}>Approved</button>
+
+<button onClick={()=>SetFilter("Not Approved")}  className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter ==='Not Approved'? "text-[#3EA6A9]": "text-gray-700"}`}>Not Approved</button>
+</div>
+      </div>
+
+<div className='relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide'>
+<table className='w-full text-sm text-gray-500'>
+   <thead className='text-xs text-gray-700 text-left uppercase'>
+    <tr>
+      <th scope='col' className='px-6 py-3'> Blog Title & Comments</th>
+      <th scope='col' className='px-6 py-3 max-sm:hidden'> Date</th>
+      <th scope='col' className='px-6 py-3'>Action</th>
+    </tr>
+   </thead>
+   <tbody>
+
+{comments
+  .filter(comment => filter === "Approved" ? comment.isApproved === true : comment.isApproved === false)
+  .map((comment, index) => (
+    <CommentTable
+      key={comment._id}
+      comment={comment}
+      index={index + 1}
+      fetchComment={fetchComments}
+    />
+  ))
+}
+
+   </tbody>
+</table>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Comments
