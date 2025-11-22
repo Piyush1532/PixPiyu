b627c4b64093cf5a28b40bd0ebe134f59931eb80
Piyush1532 <piyushadav503@gmail.com>
2025-11-22 13:03:28 +0530

    added new routes

--- Server/routes/adminRoute.js
+++ Server/routes/adminRoute.js
@@ -1,9 +1,19 @@
 import express from "express"
-import { adminLogin } from "../controllers/adminController.js"
+import { adminLogin, approveCommentById, getAllBlogs, getAllComments, getDashBoard } from "../controllers/adminController.js"
+import auth from "../middleware/auth.js"
+import { deleteBlogById } from "../controllers/blogController.js"
 
 const adminRouter=express.Router()
 
+
 adminRouter.post("/login",adminLogin)
+adminRouter.get("/comments",auth,getAllComments)
+adminRouter.get("/blog",auth,getAllBlogs)
+adminRouter.post("/delete-comment",auth,deleteBlogById)
+adminRouter.post("/approve-comment",auth,approveCommentById)
+adminRouter.get("/dashboard",auth,getDashBoard)
+
+
 
 
 export default adminRouter
