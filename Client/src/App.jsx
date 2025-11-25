560934c054d42c0a7275c1a1aadd6c376572f521
Piyush1532 <piyushadav503@gmail.com>
2025-11-25 14:18:10 +0530

    added toster

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,15 +9,21 @@ import ListBlog from './Pages/Admin/ListBlog'
 import Comments from './Pages/Admin/Comments'
 import Login from './Components/Admin/Login'
 import "quill/dist/quill.snow.css"
-
+import {Toaster} from "react-hot-toast"
+import { useAppContext } from './Context/AppContext'
 const App = () => {
+
+  const{token}=useAppContext()
+
+
   return (
     <div>
+      <Toaster/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/blog/:blogId" element={<Blog/>}/>
 
-        <Route path='/admin'element={true ?<Layout/>:<Login/>}>
+        <Route path='/admin'element={token ?<Layout/>:<Login/>}>
 <Route index element={<Dashboard/>}/>
 <Route path="addblog" element={<AddBlog/>}/>
 <Route path="listblog" element={<ListBlog/>}/>
