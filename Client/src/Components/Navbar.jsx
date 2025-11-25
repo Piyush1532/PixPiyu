a13e7bd5476796c1011e640a7c5b2e3cf2c31056
Piyush1532 <piyushadav503@gmail.com>
2025-11-25 14:20:28 +0530

    updated

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../Context/AppContext'
 
 const Navbar = () => {
 
-    const navigate=useNavigate()
-
-
+    const {navigate,token}=useAppContext()
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 '>
       <img src={assets.logo} alt="logo" className='w-10 sm:w-44' onClick={()=>navigate("/")}/>
-      <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-[#3EA6A9] text-white px-10 py-2.5' onClick={()=>navigate("/admin")}>Login 
+      <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-[#3EA6A9] text-white px-10 py-2.5' onClick={()=>navigate("/admin")}>
+        {token? "Dashboard":"Login"} 
         <img src={assets.arrow} alt="arrow" className='w-3' />
       </button>
     </div>
