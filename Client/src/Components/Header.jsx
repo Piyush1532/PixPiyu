1f87869c6d32003a7313d7ac3dde34ef5d00b1d6
Piyush1532 <piyushadav503@gmail.com>
2025-11-25 14:20:14 +0530

    created on submit handler function

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../Context/AppContext'
 
 const Header = () => {
+
+const {setInput,input}=useAppContext()
+const inputRef=useRef()
+
+const onSubmitHandler=async (e) => {
+ e.preventDefault()
+ setInput(inputRef.current.value) 
+}
+
+const onClear=()=>{
+  setInput("")
+  inputRef.current.value=""
+}
+
+
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
       <div className='text-center mt-20 mb-8'>
@@ -15,10 +31,17 @@ const Header = () => {
 
  <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>This is your space to think out loud, to share what matters, and to write without filters. Whether it's one word or a thousand, your story starts right here.</p>
 
- <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
-  <input type="text" name='' placeholder='Search For Blog ' required className='w-full pl-4 outline-none'/>
+ <form onSubmit={onSubmitHandler} className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+  <input type="text" name='' placeholder='Search For Blog ' required className='w-full pl-4 outline-none' ref={inputRef}/>
   <button type='submit' className='bg-[#3EA6A9] text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
  </form>
+      </div>
+      <div className='text-center'>
+        {input && 
+            <button className='border font-light text-sm py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer' onClick={onClear}>Clear Search</button>
+            
+        }
+   
       </div>
       <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50'/>
     </div>
