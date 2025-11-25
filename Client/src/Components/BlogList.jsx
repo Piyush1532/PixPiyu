25d3c505ee58ad3a5c93534ea0b231b0012c56d7
Piyush1532 <piyushadav503@gmail.com>
2025-11-25 14:19:49 +0530

    created filterblogs function

--- Client/src/Components/BlogList.jsx
+++ Client/src/Components/BlogList.jsx
@@ -2,10 +2,19 @@ import React, { useState } from 'react'
 import { blog_data, blogCategories } from '../assets/assets'
 import {motion} from "motion/react"
 import BlogCard from './BlogCard'
+import { useAppContext } from '../Context/AppContext'
 const BlogList = () => {
 
     const [category,SetCategory]=useState("All")
+    const {blogs,input}=useAppContext()
 
+
+    const filteredBlogs=()=>{
+      if (input==="") {
+        return blogs
+      }
+      return blogs.filter((blog)=>blog.title.toLowerCase().includes(input.toLowerCase()) || blog.category.toLowerCase().includes(input.toLowerCase()))
+    }
 
   return (
     <div>
@@ -27,7 +36,7 @@ const BlogList = () => {
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
         {/* blog cards */}
-        {blog_data.filter((blog)=>category==="All" ?true :blog.category===category).map((blog)=>(<BlogCard key={blog._id} blog={blog}/>))}
+        {filteredBlogs().filter((blog)=>category==="All" ?true :blog.category===category).map((blog)=>(<BlogCard key={blog._id} blog={blog}/>))}
 
       </div>
     </div>
