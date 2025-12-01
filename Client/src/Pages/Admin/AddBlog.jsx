deb36f93d03a2dedd7c7a822869e684bfefcda42
Piyush1532 <piyushadav503@gmail.com>
2025-12-01 13:42:10 +0530

    created onsubmithandler function to add blog

--- Client/src/Pages/Admin/AddBlog.jsx
+++ Client/src/Pages/Admin/AddBlog.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill'
+import { useAppContext } from '../../Context/AppContext'
+import toast from 'react-hot-toast'
 
 const AddBlog = () => {
+
+  const {axios}=useAppContext()
+  const [isAdding,setIsAdding]=useState(false)
+
+
 const [image,setImage]=useState(false)
 const [title,setTitle]=useState("")
 const [subTitle,setSubTitle]=useState("")
@@ -13,7 +20,38 @@ const editorRef=useRef(null)
 const quillRef=useRef(null)
 
 const onSubmitHandler=async (e) => {
-  e.preventDefault()
+  try {
+    e.preventDefault()
+    setIsAdding(true)
+    const blog={
+      title,
+      subTitle,
+      description:quillRef.current.root.innerHTML,
+      category,
+      isPublished
+    }
+
+    const formData=new FormData()
+    formData.append("blog",JSON.stringify(blog))
+    formData.append("image",image)
+
+    const {data}=await axios.post("/api/blog/add",formData)
+    if (data.success) {
+      toast.success(data.message)
+      setImage(false)
+      setTitle("")
+      quillRef.current.root.innerHTML=""
+      setCategory('Startup')
+
+    }
+    else{
+toast.error(data.message)
+    }
+  } catch (error) {
+    toast.error(error.message)
+  }finally{
+    setIsAdding(false)
+  }
 }
 
 const generateContent=async () => {
@@ -35,7 +73,6 @@ if (!quillRef.current && editorRef.current) {
   <img src={!image  ?assets.upload_area :URL.createObjectURL(image)} alt="" className='mt-2 h-16 rounded cursor-pointer'/>
   <input type="file" id='image' hidden  required onChange={(e)=>setImage(e.target.files[0])}/>
 </label>
-
 
 <p className='mt-4'>Blog Title</p>
 <input type="text" placeholder='Type Here' required className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' onChange={(e)=>setTitle(e.target.value)} value={title}/>
@@ -64,7 +101,7 @@ max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
   <input type="checkbox" checked={isPublished} className='scale-125 cursor-pointer' onChange={e=>setIsPublished(e.target.checked)}/>
 </div>
 
-<button type='submit' className='mt-8 w-40 h-10 bg-[#3EA6A9] text-white rounded cursor-pointer text-sm'>Add Blog</button>
+<button type='submit' className='mt-8 w-40 h-10 bg-[#3EA6A9] text-white rounded cursor-pointer text-sm' disabled={isAdding}>{isAdding ? "Adding...":"Add Blog"}</button>
 
       </div>
 
